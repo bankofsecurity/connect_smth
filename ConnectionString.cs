eadd9f932a393bc8adc27cfcd88c7f79b1f66b54
bankofsecurity <89392276+bankofsecurity@users.noreply.github.com>
2021-08-23 14:54:27 +0300

    Create ConnectionString.cs

---
+++ ConnectionString.cs
@@ -0,0 +1,6 @@
+<connectionStrings>  
+  <add
+    name="sqlServer"
+    providerName="System.Data.SqlClient"
+    connectionString="Data Source=localhost; Initial Catalog=MyDatabase; User Id=bankofsecurity; Password=bankofsecurity12312312;" />
+</connectionStrings>
